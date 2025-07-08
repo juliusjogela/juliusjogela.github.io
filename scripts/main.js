d6da93146687fe834b6c510847858857e36dfd20
Julius Jogela <jogelaj@tcd.ie>
2025-07-08 01:29:38 +0100

    Updates to website

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
+const myImage = document.getElementById("myPic");
+
+myImage.addEventListener("click", () => {
+  const currentSrc = myImage.getAttribute("src");
+  if (currentSrc === "images/me.jpg") {
+    myImage.setAttribute("src", "images/passportjulius.jpg");
+  } else {
+    myImage.setAttribute("src", "images/me.jpg");
+  }
+});
 
 
